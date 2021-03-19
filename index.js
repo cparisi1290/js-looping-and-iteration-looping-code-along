742ff5079d4aad748978408dab5cd8ec2a655d17
Cassandra <cparisi1290@gmail.com>
2021-03-19 10:36:25 -0400

    commit tree 09ecfd6ccb6e646d90da33080a28f8fb8e15f183

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(names, occasion ) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(startingNumber){
+    while ( startingNumber > 0 ) {
+        console.log( startingNumber );
+        startingNumber -= 1
+    }
+    console.log( startingNumber );
+}
